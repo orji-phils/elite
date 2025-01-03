251ae518a66f06d7c608b14526bb5316ced4dead
Philemon <orjiphil@gmail.com>
2025-01-03 22:31:21 +0100

    Requires all important files for smooth running of the application

---
+++ php/helperFiles/allFileImport.php
@@ -0,0 +1,10 @@
+<?php // allFileImport.php
+require_once "../helperFiles/htmlHead.php";
+require_once "../helperFiles/forms.php";
+require_once "../helperFiles/validations.php";
+require_once "../databaseManagement/queryFunctions.php";
+require_once '../helperFiles/uploads.php';
+require_once '../helperFiles/configurationFile.php';
+require_once '../helperFiles/helperFunctions.php';
+require_once "../emailManagement/emailSender.php";
+?>
