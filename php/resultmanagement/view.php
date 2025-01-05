6bde3449d197e9ef5a248ef6ce4f69e338ad5234
Philemon <orjiphil@gmail.com>
2025-01-05 16:58:32 +0100

    Fixed code to show stored term and session values

--- php/resultmanagement/view.php
+++ php/resultmanagement/view.php
@@ -4,44 +4,41 @@ require_once "../helperFiles/allFileImport.php";
 // check login status
 loginStatus($_SESSION["userName"], "Please login to view results.");
 
-// determin the uuser name for data retrieval
-$userName = $_SESSION["type"] == "Teacher" ?
-$_SESSION["studentUserName"] : $_SESSION["userName"];
+// determin the user name for data retrieval
+$userName = $_SESSION["type"] == "student" ?
+$_SESSION["userName"] : $_SESSION["studentUserName"];
 
 // retrieve the term and session from the result2 table
 $retrieveInfo = sqlFunctions("SELECT DISTINCT term, session FROM result2 WHERE userName = ?", [$userName],
 "", "Unable to retrieve the term and session data to proceed, please try again later.", $pdo);
 
-// retrieve the result obtained from the database
+// retrieve the term and session values obtained from the server
 $termSession = [];
 while ($retrieve = $retrieveInfo->fetch()) {
     $termSession[] = $retrieve["term"] . "_" . $retrieve["session"];
 }
 
-// instantiate the formFields array
+// instantiate the formfields array
 $formFields = [
-    "Term_Session" => array_merge(["radio", "term_session"], $termSession)
+    "Term _ Session" => array_merge(["radio", "term_session"], $termSession)
 ];
 
 if (isset($_POST["term_session"])) {
     list($_SESSION["term"], $_SESSION["session"]) = explode("_", cleanInput($_POST["term_session"]));
 
-    // move to select subject to update page
+    // move to select subject to view page
     header("Location: viewResult.php");
     exit();
 }
 
 // display the html page
-pageHeader("View Result", $_SESSION["success"] ??"", $_SESSION["error"] ?? "", "formStyle.css");
-echo "<p>Select the term and session of the result you want to view.</p>";
+pageHeader("view Result", $_SESSION["success"] ?? "", $_SESSION["error"] ?? "", "formStyle.css");
+echo "<div>Select the term and session of the result you want to view.</div>";
 
-// display the html form
+// checks if the user selects a term and session before displaying the form
 if ($termSession) {
     htmlForms("view.php", $formFields, $fields, "Next");
 } else {
-    echo "<p>Sorry! No result has been recorded, please come back later when there's a recorded result. Thank you.</p>";
+    echo "<p>Sorry! No result has been recorded, please record a result before updating.</p>";
 }
 ?>
-
-</body>
-</html>
